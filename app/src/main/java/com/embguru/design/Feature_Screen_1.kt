3d17cb9f990b833d83f54c5bf4f04544fd88fd04
Daxesh-Italiya <daxeshitaliya58@gmail.com>
2023-02-05 23:32:48 +0530

    Update Page Setup

---
+++ app/src/main/java/com/embguru/design/Feature_Screen_1.kt
@@ -0,0 +1,11 @@
+package com.embguru.design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Feature_Screen_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature_screen1)
+    }
+}
---
+++ app/src/main/java/com/embguru/design/IntrestPage.kt
@@ -0,0 +1,11 @@
+package com.embguru.design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntrestPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intrest_page)
+    }
+}
